2de548320bc6daa3440f0bb90aa3426dfce9a992
Gabriel Durães <gduraes10@gmail.com>
2021-09-21 17:24:53 -0300

    getfile aws base64

--- plugins/aws.js
+++ plugins/aws.js
@@ -1,4 +1,5 @@
 import {GetObjectCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import S3 from 'aws-sdk/clients/s3';
 
 const s3conf = {
   accessKeyId: "AKIARZJ56O72WLO3WGIE",
@@ -7,12 +8,17 @@ const s3conf = {
 
 const client = new S3Client({credentials: s3conf, region: "sa-east-1"})
 
+let s3 = new S3({
+  accessKeyId: "AKIARZJ56O72WLO3WGIE",
+  secretAccessKey: "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",
+  region: 'sa-east-1'
+});
+
 export default function () {
   return {
     getFile: async function (Key = 'arquivos/eu.jpg') {
-      const getObjectCommand = new GetObjectCommand({Bucket: 'masters-lms-bucket', Key})
-      const response = await client.send(getObjectCommand);
-      console.log(response.Body)
+      const data = await s3.getObject({Bucket: 'masters-lms-bucket', Key}).promise();
+      return data.Body.toString('base64');    
     },
     postFile: async function(directory, file) {
       const putObjectCommand = new PutObjectCommand({
